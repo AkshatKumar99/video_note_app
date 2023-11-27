2475ab9e047ac42dfa0b87406bec7a2f4b4c5d8f
AkshatKumar99 <kumar.aks99@gmail.com>
2023-11-26 23:29:57 -0600

    changed layout to make video bigger

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ col1, col2 = st.columns([0.3, 0.6])
 
 with col1:
     st.subheader("Paste in URL to YouTube Video:")
-    url = st.text_input('YouTube URL')
+    url = st.text_area("URL:")
+    #url = st.text_input('YouTube URL')
+    transcript = get_youtube_transcript(url)
 
 with col2:
-    transcript = get_youtube_transcript(url)
-    with st.expander("Retrieved Transcript:"):
-        
+    video_container = st.container()
+    try:
+        video_container.video(url)
+    except:
+        video_container.write('No URL Entered! Enter URL to display video here.')
+    with st.expander("Transcript Stats:"):
         tr = f"""{transcript}"""
-        st.write(tr)
+        #st.write(tr)
+        st.write(f"Length: {len(transcript)} words")
