ac6ae7a342eb4b2ae33d146120237547265e5e54
AkshatKumar99 <kumar.aks99@gmail.com>
2023-11-26 22:42:11 -0600

    added function to parse transcript

--- helper_functions.py
+++ helper_functions.py
@@ -4,7 +4,14 @@ def get_youtube_transcript(video_url):
     try:
         video_id = video_url.split('v=')[1]
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        return transcript
+        return parse_transcript(transcript)
     except Exception as e:
         return f"Error: {str(e)}"
         
+def parse_transcript(transcript):
+    
+    text = ""
+    for current_section in transcript:
+        text+=current_section["text"]
+        text+=" "
+    return text
